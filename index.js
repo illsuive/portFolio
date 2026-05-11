2a751e4717e0120fa5f2740b39abc3ae1f738707
Gautam <gautambutola@outlook.com>
2026-05-11 08:23:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ const FullStackProject = [
         name: 'food-reel',
         img: 'imgs/food-reel.png',
         description: 'A food discovery application built with React.js for exploring and sharing delicious recipes. using scroll ',
-        ProjectDeployed: 'https://dummy-front-six.vercel.app',
+        ProjectDeployed: 'https://dummy-front-six.vercel.app/login',
         github: 'https://github.com/illsuive/foodReelBackend'
     },
     {
